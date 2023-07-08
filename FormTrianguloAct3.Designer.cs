f0359e3b86309ed3058892295aa387af06d1c648
Allison Tapia <tapioca204@hotmail.com>
2023-07-08 14:44:22 -0700

    Agregando try catch para errores y sumatoria de lados

--- FormTrianguloAct3.Designer.cs
+++ FormTrianguloAct3.Designer.cs
@@ -81,10 +81,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(36, 304);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(90, 38);
             this.button1.TabIndex = 4;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
--- FormTrianguloAct3.cs
+++ FormTrianguloAct3.cs
@@ -16,5 +16,21 @@ namespace ProgramacionOrientadaAObjetos
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Holaaa");
+            //MessageBox.Show(textBox1.Text + " " + textBox2.Text);
+            //MessageBox.Show(textBox1.Text + textBox2.Text);
+            try{
+                int lado1 = Convert.ToInt32(textBox1.Text);
+                int lado2 = Convert.ToInt32(textBox2.Text);
+                int lado3 = Convert.ToInt32(textBox3.Text);
+                MessageBox.Show((lado1 + lado2 + lado3).ToString());
+            }catch(Exception Error){
+                MessageBox.Show("Error", "Errorsito", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
     }
 }
